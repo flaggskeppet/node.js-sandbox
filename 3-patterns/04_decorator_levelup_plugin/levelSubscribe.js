40245b555fbc6ea1cf5a5304c20f00cb298534b1
Jonas Mattsson <jonas.mattsson@verisure.com>
2018-05-31 16:38:37 +0200

    Added decorator

--- 3-patterns/04_decorator_levelup_plugin/levelSubscribe.js
+++ 3-patterns/04_decorator_levelup_plugin/levelSubscribe.js
@@ -1,14 +1,15 @@
 "use strict";
 
-module.exports = function levelSubscribe(db) {
-  db.subscribe = (pattern, listener) => {       //[1]
-    db.on('put', (key, val) => {         //[2]
+module.exports = function levelSubscribe(db) { // Factory function taking a db-instance
+  // Decorate the db object with a new method taking a pattern and a callback function
+  db.subscribe = (pattern, listener) => {       
+     db.on('put', (key, val) => {         // Listen for put events on the db object
       const match = Object.keys(pattern).every(
-        k => (pattern[k] === val[k])     //[3]
+        k => (pattern[k] === val[k])     // Pattern matching
       );
       
       if(match) {
-        listener(key, val);            //[4]
+        listener(key, val);            // Trigger the callback function if matching
       }
     });
   };
--- 3-patterns/04_decorator_levelup_plugin/levelSubscribeTest.js
+++ 3-patterns/04_decorator_levelup_plugin/levelSubscribeTest.js
@@ -1,15 +1,18 @@
 "use strict";
 
-const level = require('level');           //[1]
-const levelSubscribe = require('./levelSubscribe');     //[2]
+const level = require('level');           
+const levelSubscribe = require('./levelSubscribe');     
 
 let db = level(__dirname + '/db', {valueEncoding: 'json'});
 
+// Decorate the leveldb instance
 db = levelSubscribe(db);
-db.subscribe(
-  {doctype: 'tweet', language: 'en'},     //[3]
-  (k, val) => console.log(val)
+
+// Use the new method on the augmented object
+db.subscribe( 
+  {doctype: 'tweet', language: 'en'}, // Pattern
+  (k, val) => console.log(val) // Listener
 );
 
-db.put('1', {doctype: 'tweet', text: 'Hi', language: 'en'}); //[4]
+db.put('1', {doctype: 'tweet', text: 'Hi', language: 'en'}); 
 db.put('2', {doctype: 'company', name: 'ACME Co.'});
