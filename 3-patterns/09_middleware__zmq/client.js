bb8e003ca5b96616d199f74972a29a5a99507496
Jonas Mattsson <jonas.mattsson@verisure.com>
2018-06-07 16:24:34 +0200

    added middleware example

--- 3-patterns/09_middleware__zmq/client.js
+++ 3-patterns/09_middleware__zmq/client.js
@@ -7,9 +7,13 @@ const jsonMiddleware = require('./jsonMiddleware');
 const request = zmq.socket('req');
 request.connect('tcp://127.0.0.1:5000');
 
+// Wrap the connected socket in the MiddlewareManager
 const zmqm = new ZmqMiddlewareManager(request);
 
+// Execute the .json() factory method on the jsonMiddleWare module
 zmqm.use(jsonMiddleware.json());
+
+// 
 zmqm.use({
   inbound: function (message, next) {
     console.log('Echoed back: ', message.data);
--- 3-patterns/09_middleware__zmq/jsonMiddleware.js
+++ 3-patterns/09_middleware__zmq/jsonMiddleware.js
@@ -1,7 +1,14 @@
 "use strict";
 
+/*
+Implementation of a middleware that serialized and deserializes json messages
+Note that the middleware is a factory: it´s a function that returns an object with the
+two middleware functions.
+*/
 module.exports.json = () => {
   return {
+    // Serializes the message received as input and assigns the result back to the data property of message,
+    // allowing further processing along the pipeline.
     inbound: function (message, next) {
       message.data = JSON.parse(message.data.toString());
       next();
